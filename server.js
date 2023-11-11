5cf0be56a072d1929c9a0d3b13ddd9d2d23b9a41
Nguyen Vo <vophannguyen@gmail.com>
2023-11-10 22:28:44 -0500

    fixed1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./api/app");
 const init = async () => {
   // await seed();
-  const PORT = 6001;
+  const PORT = process.env.URL || 6001;
   app.listen(PORT, () => `Listen ${PORT}`);
 };
 init();
