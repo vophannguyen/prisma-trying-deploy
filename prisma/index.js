52bc089cb67657b596baa0008fbd148407b256bd
Nguyen Vo <vophannguyen@gmail.com>
2023-11-10 22:17:32 -0500

    fixed-4

--- prisma/index.js
+++ prisma/index.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require("../src/generated/client");
 const bcrypt = require("bcrypt");
 const prisma = new PrismaClient().$extends({
   query: {
